45c07bccf616abd4dd9109a151dd2d4e66ae47a7
Alex-Sorokolit <sorokolital@gmail.com>
2022-07-15 23:11:02 +0300

    pagination

--- src/sass/js/review-slider-setup.js
+++ src/sass/js/review-slider-setup.js
@@ -1,13 +1,20 @@
     var swiper = new Swiper('.mySwiper', {
-        // cssMode: true,
-        // navigation: {
-        // nextEl: '.swiper-button-next',
-        // prevEl: '.swiper-button-prev',
-        // },
-        pagination: {
-          el: ".swiper-pagination",
+      // cssMode: true,
+      // navigation: {
+      // nextEl: '.swiper-button-next',
+      // prevEl: '.swiper-button-prev',
+      // },
+      speed: 1500,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      loop: true,
+
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
         },
-        
-        mousewheel: true,
-        keyboard: true,
+      mousewheel: true,
+      keyboard: true,
     });
